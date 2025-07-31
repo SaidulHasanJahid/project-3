f1875bdbd00ac98073d9de8e4fc3fd30f2229c2b
SaidulHasanJahid <jahid2002@gmail.com>
2025-07-31 20:57:41 +0600

    hsulkslhsivhsncvcnbnl

--- @modules/@common/layouts/header/top-bar/index.tsx
+++ @modules/@common/layouts/header/top-bar/index.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaChevronDown, FaGlobe, FaUser } from "react-icons/fa";
-
 const TopBar = () => {
+  const currencies = ['USD', 'BDT', 'INR', 'EURO'];
+  const [selectedCurrency, setSelectedCurrency] = useState('USD');
+const languages = ['English', 'Spanish', 'French', 'Bangla'];
+  const [selectedLang, setSelectedLang] = useState('English');
   return (
     <div className="flex justify-between items-center text-[15px] text-[#767678] max-[1000px]:hidden">
       {/* Left side */}
@@ -10,32 +13,55 @@ const TopBar = () => {
       {/* Right side */}
       <div className="flex gap-4 sm:gap-6 items-center relative z-50">
         {/* Language dropdown */}
-        <div className="relative group cursor-pointer">
-          <div className="flex items-center gap-2 sm:gap-4 pr-4 border-r border-[#767678da] h-[25px]">
-            <FaGlobe /> English <FaChevronDown className="text-xs" />
-          </div>
-          <ul className="absolute top-full left-0 mt-2 w-28 bg-white shadow-md p-2 text-[13px] text-center rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 ease-in-out">
-            {["English", "Spanish", "French", "Bangla"].map((lang, i) => (
-              <li key={i} className="py-1 hover:text-black cursor-pointer">
-                {lang}
-              </li>
-            ))}
-          </ul>
-        </div>
 
-        {/* Currency dropdown */}
-        <div className="relative group cursor-pointer">
-          <div className="flex items-center gap-2 sm:gap-4 pr-4 border-r border-[#767678d2] h-[25px]">
-            $ USD <FaChevronDown className="text-xs" />
-          </div>
-          <ul className="absolute top-full left-0 mt-2 w-28 bg-white shadow-md px-4 py-2 text-[#444] text-[13px] text-center rounded-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 ease-in-out">
-            {["USD", "BDT", "INR", "EURO"].map((cur, i) => (
-              <li key={i} className="py-1 hover:text-black cursor-pointer">
-                {cur}
-              </li>
-            ))}
-          </ul>
-        </div>
+
+     <div className="relative group cursor-pointer">
+      {/* Selected Currency */}
+      <div className="flex items-center gap-2 sm:gap-4  pr-4 border-r border-[#767678d2] h-[25px]">
+       <FaGlobe /> {selectedLang} <FaChevronDown className="text-xs" />
+      </div>
+
+      {/* Dropdown List */}
+      <ul className="absolute top-full left-0 mt-2 w-28 bg-white shadow-md px-4 py-2 text-[#444] text-[13px] text-center rounded-md 
+        opacity-0 invisible group-hover:opacity-100 group-hover:visible 
+        transition-all duration-300 ease-in-out z-50">
+        {languages.map((cur, i) => (
+          <li
+            key={i}
+            onClick={() => setSelectedLang(cur)}
+            className="py-1 hover:text-black cursor-pointer"
+          >
+            {cur}
+          </li>
+        ))}
+      </ul>
+    </div>
+
+
+
+
+     <div className="relative group cursor-pointer">
+      {/* Selected Currency */}
+      <div className="flex items-center gap-2 sm:gap-4 pr-4 border-r border-[#767678d2] h-[25px]">
+        $ {selectedCurrency} <FaChevronDown className="text-xs" />
+      </div>
+
+      {/* Dropdown List */}
+      <ul className="absolute top-full left-0 mt-2 w-28 bg-white shadow-md px-4 py-2 text-[#444] text-[13px] text-center rounded-md 
+        opacity-0 invisible group-hover:opacity-100 group-hover:visible 
+        transition-all duration-300 ease-in-out z-50">
+        {currencies.map((cur, i) => (
+          <li
+            key={i}
+            onClick={() => setSelectedCurrency(cur)}
+            className="py-1 hover:text-black cursor-pointer"
+          >
+            {cur}
+          </li>
+        ))}
+      </ul>
+    </div>
+
 
         {/* My Account */}
         <div className="flex items-center gap-1 cursor-pointer">
